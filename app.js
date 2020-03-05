b7aa977acad824e874917437f1a590ef622a8d4f
Potatomochi <noahrsam@gmail.com>
2020-03-06 01:02:56 +0800

    Major Overhaul, fixed mobile view

--- app.js
+++ app.js
@@ -1,30 +1,5 @@
 $(document).ready(function(){
-    const navSlide = () => {
-        const burger = document.querySelector('.burger');
-        const nav = document.querySelector('.nav-links');
-        const navLinks = document.querySelectorAll('.nav-links li');
-    //Menu Toggler//
-        burger.addEventListener('click',() => {
-            nav.classList.toggle('nav-active');
     
-    // Links Animate here // 
-            navLinks.forEach((link,index)=>{
-                if(link.style.animation){
-                    link.style.animation = ''
-                }
-                else{
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
-                }
-                
-            });    
-            burger.classList.toggle('toggle');
-            
-        });
-        
-        
-    }
-    navSlide();
-
 
     $('#portfolio').mouseenter(function(){
         $('.text').addClass("animated slideInLeft");
@@ -33,13 +8,6 @@ $(document).ready(function(){
         $('.text').removeClass("moggle");
         $('.text').addClass("moggle-exit");
     })
-
-
-
-
-    navLinks.hover(function(){
-        navLinks.addClass("navhover")
-    });
 
 
 });
